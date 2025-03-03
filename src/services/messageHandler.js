2e44a8c8aa024b05dc9b5da5d018016c4570c36f
Wkatir <110741874+wkatir@users.noreply.github.com.>
2025-03-03 15:19:56 -0600

    feat: add a menu to handle options

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -5,13 +5,17 @@ class MessageHandler {
     if (message?.type === 'text') {
       const incomingMessage = message.text.body.toLowerCase().trim();
 
-      if(this.isGreeting(incomingMessage)){
+      if (this.isGreeting(incomingMessage)) {
         await this.sendWelcomeMessage(message.from, message.id, senderInfo);
         await this.sendWelcomeMenu(message.from);
       } else {
         const response = `Echo: ${message.text.body}`;
         await whatsappService.sendMessage(message.from, response, message.id);
       }
+      await whatsappService.markAsRead(message.id);
+    } else if (message?.type === "interactive") {
+      const option = message?.interactive?.button_reply?.id;
+      await this.handleMenuOption(message.from, option);
       await whatsappService.markAsRead(message.id);
     }
   }
@@ -22,7 +26,7 @@ class MessageHandler {
   }
 
   getSenderName(senderInfo) {
-    return senderInfo.profile?.name || senderInfo.wa_id;
+    return senderInfo?.profile?.name || senderInfo?.wa_id || "Usuario";
   }
 
   async sendWelcomeMessage(to, messageId, senderInfo) {
@@ -38,14 +42,35 @@ class MessageHandler {
         type: 'reply', reply: { id: 'option_1', title: 'Agendar' }
       },
       {
-        type: 'reply', reply: { id: 'option_2', title: 'Consultar'}
+        type: 'reply', reply: { id: 'option_2', title: 'Consultar' }
       },
       {
-        type: 'reply', reply: { id: 'option_3', title: 'Ubicación'}
+        type: 'reply', reply: { id: 'option_3', title: 'Ubicación' }
       }
     ];
 
     await whatsappService.sendInteractiveButtons(to, menuMessage, buttons);
+  }
+
+  async handleMenuOption(to, option) {
+    let response;
+
+    switch (option) {
+      case 'option_1':
+        response = 'Agendar Cita';
+        break;
+      case 'option_2':
+        response = 'Realiza tu consulta';
+        break;
+      case 'option_3':
+        response = 'Esta es nuestra Ubicación';
+        break;
+      default:
+        response = 'Lo siento, no entendí tu selección, por favor elige una de las opciones disponibles.';
+        break;
+    }
+
+    await whatsappService.sendMessage(to, response);
   }
 
 }
