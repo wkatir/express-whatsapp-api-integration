f9ded713fdf71f5b6bb318cf709182390d29bb90
wkatir <wilmerhenrysalazarmartinez@gmail.com>
2025-03-03 10:35:14 -0600

    feat: adding a new fuction to handle greetings

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -3,14 +3,27 @@ import whatsappService from './whatsappService.js';
 class MessageHandler {
   async handleIncomingMessage(message) {
     if (message?.type === 'text') {
-      const response = `Echo: ${message.text.body}`;
-      await whatsappService.sendMessage(message.from, response, message.id);
+      const incomingMessage = message.text.body.toLowerCase().trim();
+
+      if(this.isGreeting(incomingMessage)){
+        await this.sendWelcomeMessage(message.from, message.id)
+      } else {
+        const response = `Echo: ${message.text.body}`;
+        await whatsappService.sendMessage(message.from, response, message.id);
+      }
       await whatsappService.markAsRead(message.id);
     }
   }
 
   isGreeting(message) {
-    const greetings = ['hi', 'hello', 'hey'];
+    const greetings = ["hola", "hello", "hi", "buenas tardes"];
+    return greetings.includes(message);
+  }
+
+  async sendWelcomeMessage(to, messageId) {
+    const welcomeMessage = "Hola, Bienvenido a nuestro servicio." + 
+    "¿En Qué puedo ayudarte Hoy?";
+    await whatsappService.sendMessage(to, welcomeMessage, messageId);
   }
 
 }
